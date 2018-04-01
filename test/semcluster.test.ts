6a225b020c974752477a0aad8af0e917970029fe
Michael Neely <michaeljneelysd@gmail.com>
2018-04-01 15:17:16 +0100

    stub CoreNLP server

--- test/semcluster.test.ts
+++ test/semcluster.test.ts
@@ -1,38 +1,60 @@
 import CoreNLP, { ConnectorServer } from "corenlp";
+import * as fs from "fs-extra";
+import * as path from "path";
+import * as sinon from "sinon";
 import * as request from "supertest";
 import * as app from "../src/app";
 import * as corenlpService from "../src/services/corenlp";
 import * as semClusterService from "../src/services/semcluster";
 import { Stack } from "../src/utils/stack";
 
-const cteText = "Cat. Cats. Americas. George Washington. John in the hat. This is minty gum. John of Smith. John Smith of. John Smith West. John of Smith West. Dog bird dog. This is minty not gum. James of minty. gold medal the. the. running. spicy gum. fresh gum stick. Michael. Michael running.";
-const testString = "This is a sentence";
-
 describe("TEST SemCluster Service", () => {
 
-    beforeEach(function() {
-        // Long for CI builds
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+    // Simple sentence to test string building from stack
+    const testString = "This is a sentence";
+    // Complex document to test every branch of the candidate term extraction function
+    const cteText = "Cat. Cats. Americas. George Washington. John in the hat. This is minty gum. John of Smith. John Smith of. John Smith West. John of Smith West. Dog bird dog. This is minty not gum. James of minty. gold medal the. the. running. spicy gum. fresh gum stick. Michael. Michael running.";
+    // Stubbing CoreNLP Server saves a substantial amount of time
+    const coreNLPStub = sinon.stub(corenlpService, "parseDocument");
+
+    afterEach(() => {
+        coreNLPStub.reset();
     });
 
     it("Should correctly build a string from a Token Stack", async () => {
         expect.assertions(1);
-        const connector = new ConnectorServer({ dsn: process.env.CoreNLP_ADDRESS });
-        const tokenStack = new Stack<CoreNLP.simple.Token>();
-        const result = await corenlpService.parseDocument(testString, false, connector);
+        // Stub parseDocument
+        const simpleSentence = await fs.readJSON(path.join(__dirname, "mocks/simple.json"));
+        const simpleDocument = CoreNLP.simple.Document.fromJSON(simpleSentence);
+        coreNLPStub.returns({
+            document: simpleDocument,
+            speakers: new Array<string>()
+        });
+        const result = await corenlpService.parseDocument(testString, false);
         const document = result.document;
+        // Build token stack
+        const tokenStack = new Stack<CoreNLP.simple.Token>();
         for (const sentence of document.sentences()) {
             sentence.tokens().forEach((token) => {
-            tokenStack.push(token);
+                tokenStack.push(token);
             });
         }
+        // Verify behavior
         expect(semClusterService.buildStringFromTokenStack(tokenStack)).toEqual(testString);
+        coreNLPStub.reset();
     });
 
     it("Should correctly identify all unique candidate terms", async () => {
         expect.assertions(19);
-        const connector = new ConnectorServer({ dsn: process.env.CoreNLP_ADDRESS });
-        const result = await corenlpService.parseDocument(cteText, false, connector);
+        // Stub parseDocument
+        const complexSentences = await fs.readJSON(path.join(__dirname, "mocks/complex.json"));
+        const complexDocument = CoreNLP.simple.Document.fromJSON(complexSentences);
+        coreNLPStub.returns({
+            document: complexDocument,
+            speakers: new Array<string>()
+        });
+        const result = await corenlpService.parseDocument(cteText, false);
+        // Extract candidate terms and verify
         const candidateTerms = semClusterService.extractCandidateTerms(result.document);
         expect(candidateTerms.length).toEqual(18);
         expect(candidateTerms).toContain("Cat");
@@ -53,5 +75,6 @@ describe("TEST SemCluster Service", () => {
         expect(candidateTerms).toContain("spicy gum");
         expect(candidateTerms).toContain("fresh gum stick");
         expect(candidateTerms).toContain("Michael");
+        coreNLPStub.reset();
     });
 });
